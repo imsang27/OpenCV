d126f3901bd4b8fe9d1614dd68be8ccad004e610
imsang27 <69525587+imsang27@users.noreply.github.com>
2024-10-15 19:55:33 +0900

    Change Gaussian function to 1D and 2D each

--- Homework #2 - 1, 2차원 가우시안 함수.py
+++ Homework #2 - 1, 2차원 가우시안 함수.py
@@ -1,43 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm, multivariate_normal
+from mpl_toolkits.mplot3d import Axes3D
 
-# 평균과 표준편차 설정
-mu, sigma = 0, 2
+# 1차원 가우시안 함수
+def gaussian_1d(x, sigma):
+    return (1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(- (x**2) / (2 * sigma**2))
 
-# x 값 생성
+# 2차원 가우시안 함수
+def gaussian_2d(x, y, sigma):
+    return (1 / (2 * np.pi * sigma**2)) * np.exp(- (x**2 + y**2) / (2 * sigma**2))
+
+# 1차원 가우시안 그리기
 x = np.linspace(-6, 6, 100)
+sigma = 1.0
+y_1d = gaussian_1d(x, sigma)
 
-# y 값 계산 (scipy.stats.norm.pdf 사용)
-y = norm.pdf(x, mu, sigma)
+plt.figure(figsize=(10, 5))
 
-# 그래프 그리기
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('Probability density')
+# 1차원 가우시안 함수 그래프
+plt.subplot(1, 2, 1)
+plt.plot(x, y_1d, color='b')
+plt.title('1D')
+plt.grid(True)
 
-# 평균 벡터와 공분산 행렬 설정
-mean = np.array([0, 0])
-cov = np.array([[1, 0], [0, 2]])
+# 2차원 가우시안 그리기
+x_2d = np.linspace(-6, 6, 100)
+y_2d = np.linspace(-6, 6, 100)
+X, Y = np.meshgrid(x_2d, y_2d)
+Z = gaussian_2d(X, Y, sigma)
 
-# x, y 값 생성
-x, y = np.meshgrid(np.linspace(-6, 6, 100), np.linspace(-6, 6, 100))
+# 3D 그래프 생성
+ax = plt.subplot(1, 2, 2, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+ax.set_title('2D')
 
-# 2차원 좌표를 1차원 벡터로 변환
-pos = np.empty(x.shape + (2,))
-pos[:, :, 0] = x
-pos[:, :, 1] = y
-
-# 확률 밀도 계산 (scipy.stats.multivariate_normal.pdf 사용)
-rv = multivariate_normal(mean, cov)
-z = rv.pdf(pos)
-
-# 3차원 그래프 그리기
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z, cmap='viridis', linewidth=0)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
+plt.tight_layout()
 plt.show()
